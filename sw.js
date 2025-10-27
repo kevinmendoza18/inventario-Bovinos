9b9ea5c4a20094f1b28551ffc764b6d8308ca995
kwevin_mendoza <kevin.obed1999@gmail.com>
2025-10-26 23:14:22 -0500

    Actualizo rutas y archivos para PWA correcta en GitHub Pages

--- sw.js
+++ sw.js
@@ -2,17 +2,17 @@ self.addEventListener('install', function(e) {
     e.waitUntil(
       caches.open('inventario-bovinos').then(function(cache) {
         return cache.addAll([
-          '/',
-          '/index.html',
-          '/manifest.json',
-          '/sw.js',
-          // agrega aquí otros archivos: css, íconos, etc.
-        ]);
+            '/inventario-Bovinos/',
+            '/inventario-Bovinos/index.html',
+            '/inventario-Bovinos/manifest.json',
+            '/inventario-Bovinos/sw.js',
+            // añade CSS, iconos, etc. con rutas absolutas aquí
+          ]);
       })
     );
   });
   
-  
+
   self.addEventListener('fetch', function(e) {
     e.respondWith(
       caches.match(e.request).then(function(response) {
