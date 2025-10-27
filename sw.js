53d39a5de0a13a6dd53650bd3d3ba4392b35a2b7
santtiagocristancho <115848225+santtiagocristancho@users.noreply.github.com>
2025-10-26 22:03:46 -0500

    invetario Bovinos

---
+++ sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+      caches.open('inventario-bovinos').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/manifest.json',
+          '/sw.js',
+          // agrega aquí otros archivos: css, íconos, etc.
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(e) {
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  });
+  
